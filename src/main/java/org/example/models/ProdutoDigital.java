f3f67f27e36c79c510215cf4c15d7f9c62a1c026
Rafael <rafaelag-faria@hotmail.com>
2023-06-09 18:36:58 -0300

    test

--- src/main/java/org/example/models/ProdutoDigital.java
+++ src/main/java/org/example/models/ProdutoDigital.java
@@ -1,4 +1,37 @@
 package org.example.models;
 
-public class ProdutoDigital {
+public class ProdutoDigital extends Produto{
+
+    private String formato;
+    private int tamImagem;
+
+    public ProdutoDigital(int codigo, String nome, String descricao, double preco, int quantidade) {
+        super(codigo, nome, descricao, preco, quantidade);
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public int getTamImagem() {
+        return tamImagem;
+    }
+
+    public void setTamImagem(int tamImagem) {
+        this.tamImagem = tamImagem;
+    }
+    public void lisProdutoDigital(){
+        System.out.println("codigo "+ this.getCodigo());
+        System.out.println("Nome " + this.getNome());
+        System.out.println("descição" + this.getDescricao());
+        System.out.println("Preco "+ this.getPreco());
+        System.out.println("Quantidade " + this.getQuantidade());
+        System.out.println("Formato" + this.getFormato());
+        System.out.println("Tamanho "+ this.getTamImagem());
+
+    }
 }
