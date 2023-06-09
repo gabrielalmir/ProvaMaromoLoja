f4783619bda500021960341c6a888efb0ac00520
TiagoKblo <kblotiago@gmail.com>
2023-06-09 18:06:09 -0300

    Alterando Produto Fisico

--- src/main/java/org/example/models/ProdutoFisico.java
+++ src/main/java/org/example/models/ProdutoFisico.java
@@ -3,8 +3,22 @@ package org.example.models;
 public class ProdutoFisico extends Produto {
     private String peso;
     private String dimensoes;
-    public ProdutoFisico(int codigo, String nome, String descricao, double preco, int quantidade) {
+    public ProdutoFisico(int codigo, String nome, String descricao, double preco, int quantidade, String peso, String dimensoes) {
         super(codigo, nome, descricao, preco, quantidade);
+        this.peso = peso;
+        this.dimensoes = dimensoes;
+    }
+    public String getPeso() {
+        return peso;
+    }
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+    public String getDimensoes() {
+        return dimensoes;
+    }
+    public void setDimensoes(String dimensoes) {
+        this.dimensoes = dimensoes;
     }
 }
 
