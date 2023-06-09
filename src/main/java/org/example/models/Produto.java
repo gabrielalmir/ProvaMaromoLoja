033199d797379d7c716879d17609fe518b970714
momentoalmir <momentoalmir@gmail.com>
2023-06-09 17:35:07 -0300

    Atualizar classe de produtos

--- src/main/java/org/example/models/Produto.java
+++ src/main/java/org/example/models/Produto.java
@@ -1,4 +1,57 @@
 package org.example.models;
 
 public class Produto {
+    private int codigo;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int quantidade;
+
+    public Produto(int codigo, String nome, String descricao, double preco, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
