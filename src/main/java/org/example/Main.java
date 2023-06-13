9c3db622e66b059fea1457a2117a8414cba45b9d
momentoalmir <momentoalmir@gmail.com>
2023-06-13 18:03:14 -0300

    Adicionar menu principal, GerenciadorEstoque e CarrinhoDeCompras

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,56 @@
 package org.example;
 
+import org.example.views.CarrinhoDeCompras;
+import org.example.views.GerenciadorEstoque;
+
+import java.util.Scanner;
+
+//A loja deseja aprimorar seu sistema atual e expandir suas funcionalidades. O objetivo é criar
+//um sistema capaz de cadastrar produtos, exibir informações sobre os produtos, controlar o
+//estoque e processar pedidos dos clientes. Serão utilizados recursos avançados de orientação
+//a objetos, como herança, sobrescrita de métodos, tratamento de exceções e coleções.
+
+
 public class Main {
+    private final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Sistema Loja");
+        var main = new Main();
+        main.run();
+    }
+
+    public void run() {
+        var opcao = 0;
+
+        do {
+            System.out.println("========================");
+            System.out.println("\tLoja do Maromo\t");
+            System.out.println("========================");
+            System.out.println("1. Gerenciar Estoque");
+            System.out.println("2. Carrinho de compras");
+            System.out.println("3. Sair");
+            System.out.print("Digite a opção desejada: ");
+            opcao = Integer.parseInt(scanner.nextLine());
+
+            switch (opcao) {
+                case 1:
+                    var gerenciadorEstoque = new GerenciadorEstoque();
+                    gerenciadorEstoque.run();
+                    break;
+                case 2:
+                    var carrinhoDeCompras = new CarrinhoDeCompras();
+                    carrinhoDeCompras.run();
+                    break;
+                case 3:
+                    this.quit();
+                    break;
+            }
+        } while (opcao != 3);
+    }
+
+    private void quit() {
+        System.out.println("Obrigado por utilizar o sistema!");
+        System.out.println("Tudo culpa do Maromo! 😊");
+        System.exit(0);
     }
 }
---
+++ src/main/java/org/example/views/GerenciadorEstoque.java
@@ -0,0 +1,12 @@
+package org.example.views;
+
+import java.util.Scanner;
+
+public class GerenciadorEstoque {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void run() {
+        System.out.println("Gerenciador de estoque");
+        scanner.nextLine();
+    }
+}
